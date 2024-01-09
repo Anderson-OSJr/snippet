0619486b894b9d389d71e744504839bc44f378e9
Anderson OSJr <anderosj@gmail.com>
2024-01-09 18:39:01 -0300

    Few modefications to reasume the code

--- snippet/src/app/snippets/[id]/loading.tsx
+++ snippet/src/app/snippets/[id]/loading.tsx
@@ -1,7 +1,8 @@
 export default function SnippetLoadingPage() {
     return(
         <>
-            <div className="m-12 text-3xl font-bold">Loading...</div>
+            <div className="m-12 text-2xl font-bold
+                            text-center text-indigo-600">Loading...</div>
         </>
     );
 }
--- snippet/src/app/snippets/[id]/not-found.tsx
+++ snippet/src/app/snippets/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function SnippetNotFound() {
     return(
         <div className=''>
-            <h1 className="text-3xl bold my-20 mx-auto">
+            <h1 className="text-2xl bold my-20 mx-auto text-indigo-600 text-center">
                 Sorry, but we couldn&apos;t find that particular snippet...
             </h1>
         </div>
